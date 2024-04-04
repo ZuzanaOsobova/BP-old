cf142bdea7ff5a5247a3ead104821a2e8432f64f
Zuzana Osobová <osoz00@vse.cz>
2024-04-04 19:57:33 +0200

    Index & Settings css made

--- index.php
+++ index.php
@@ -1,2 +1,37 @@
 <?php
 include "header.inc.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="index_stylesheet.css">
+
+</head>
+
+<body>
+<main>
+
+    <div class="window">
+        <h2>Welcome back</h2>
+        <div class="window_text">
+
+
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum purus leo, pellentesque quis mollis auctor, tempus eu erat. Nunc porttitor ultrices libero, eget elementum nisi sodales nec. Sed id magna pellentesque, dapibus massa non, mattis sem. Fusce finibus eros tincidunt turpis pharetra posuere. Duis euismod nunc id purus congue, nec mattis nulla tempor. Nunc faucibus mi vitae massa luctus tempus. Donec porta ligula at nisi mattis lacinia. Aenean sodales tortor at congue ultricies. Fusce pulvinar ac mi id luctus. Ut nec interdum magna, et sodales leo. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque tincidunt diam nec justo cursus, a rutrum quam sagittis.
+
+            Pellentesque imperdiet sapien velit. Nunc sed tempor elit. Phasellus venenatis est quis ante imperdiet suscipit. Nam dignissim mauris ut pulvinar pellentesque. Aenean scelerisque, turpis et ullamcorper condimentum, nibh sapien pretium enim, tempus elementum neque lorem ut ipsum. Sed ac gravida nunc. Duis interdum, tellus vitae hendrerit pellentesque, massa erat luctus ex, non sagittis nibh nulla quis tellus. Nunc feugiat dolor lacinia, gravida nisl non, tincidunt dolor. Curabitur sollicitudin nibh sed dapibus vehicula. Aliquam dictum justo ut ipsum iaculis, eget posuere lorem ultrices. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+
+            Sed tincidunt metus molestie congue posuere. Cras at lorem a justo porttitor suscipit. Fusce luctus urna nec massa euismod dapibus. Curabitur vulputate rhoncus enim, iaculis auctor augue mattis ac. Donec eget erat sit amet orci faucibus tempus ut rutrum tortor. Cras dictum quis nulla id efficitur. In a felis feugiat, posuere lorem et, finibus lacus. Vestibulum ac lorem vitae augue lobortis viverra. Aliquam ipsum est, posuere tincidunt justo nec, eleifend tincidunt metus. Proin tristique risus sed dignissim euismod. Vestibulum ornare orci justo, nec ornare orci laoreet vel. Phasellus vitae urna quis nunc facilisis vestibulum. In hac habitasse platea dictumst.
+
+            Etiam pellentesque orci eu felis interdum, non rhoncus erat tempor. Maecenas interdum eros dui, ut gravida arcu venenatis non. In ullamcorper nec tortor et commodo. Quisque dictum felis id sollicitudin condimentum. Morbi sit amet iaculis enim, a rutrum lacus. Curabitur id viverra enim, ac scelerisque tortor. Cras sed ex dolor. Suspendisse in justo sollicitudin magna elementum pharetra. Nullam non dui ut sem interdum suscipit. Praesent porttitor posuere euismod. Donec tellus nisl, venenatis et elementum et, commodo sed urna. Nulla vestibulum gravida odio, volutpat tincidunt orci ultrices sed. Nunc sit amet dolor aliquam, efficitur urna sed, mattis orci. Integer tempor ligula in ante gravida, a iaculis mi iaculis.
+
+            Proin egestas lectus vitae mi vulputate, a molestie augue rutrum. Cras sodales suscipit ligula, vitae fermentum lacus fringilla non. Vivamus viverra, ligula a scelerisque ornare, magna ipsum efficitur eros, eget pellentesque risus ex bibendum libero. Morbi tellus erat, pretium et mi non, vestibulum mattis tortor. Morbi tincidunt a velit eu bibendum. Suspendisse in enim ante. Nulla aliquet quis orci et volutpat. Proin vel arcu quis dui eleifend ultricies non a metus. Duis nec laoreet metus, non lobortis metus. Proin consectetur ornare justo, id tempor dui cursus ac. Praesent non luctus libero. Duis euismod mattis nibh at lacinia. Integer tellus purus, rhoncus a lacus non, interdum scelerisque purus. Etiam venenatis, sem eu egestas iaculis, dolor lectus euismod nunc, at rutrum magna risus eu odio. Sed vel tortor libero. In vitae condimentum mauris.
+        </div>
+
+    </div>
+
+</main>
+</body>
+
+</html>
--- settings.php
+++ settings.php
@@ -1,2 +1,24 @@
 <?php
 include "header.inc.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="index_stylesheet.css">
+
+</head>
+
+<body>
+<main>
+
+    <div class="window">
+
+    </div>
+
+</main>
+</body>
+
+</html>
