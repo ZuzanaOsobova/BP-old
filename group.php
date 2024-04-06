0580437dd7900d5b6e44bc90daaba52b5b22e4e4
Zuzana Osobová <osoz00@vse.cz>
2024-04-06 22:51:35 +0200

    New Note making

--- group.php
+++ group.php
@@ -41,6 +41,17 @@ if (!empty($_POST['form_type'])){
 
     }
 
+    if ($form_type == "new_note"){
+
+        $category_id = $_POST['category_id'];
+        $group_id = $_POST['group_id'];
+        $note_name = "New Note Name";
+        $note_text = "New Note Text";
+
+        $stmt = $db->prepare("INSERT INTO notes (category_id,group_id, note_name, note_text) VALUES (?, ?, ?, ?)");
+        $stmt->execute([$category_id, $group_id, $note_name, $note_text]);
+
+    }
 }
 
 
@@ -98,14 +109,46 @@ if (!empty($_POST['form_type'])){
     <div class="column">
 
         <div class="dropdown">
-            <h2 class="dropdown-hover">Categories
+            <h2 class="dropdown-hover">
+                <?php
+
+                @$category_id = $_GET['category'];
+
+                echo "<script>console.log('$group_id, $category_id')</script>";
+
+                if (!empty($category_id)){
+                    $stmt = $db->prepare("SELECT category_name FROM categories WHERE category_id = ? LIMIT 1 ");
+                    $stmt->execute([$category_id]);
+
+                    $category_names = $stmt ->fetchAll(PDO::FETCH_ASSOC);
+
+                    if (!empty($category_names)){
+                        $category_name = $category_names[0]['category_name'];
+                        echo $category_name;
+
+                        echo '<form method="post">
+                    <input type="hidden" name="form_type" value="new_note">
+                    <input type="hidden" name="group_id" value='.$group_id.'>
+                <input type="hidden" name="category_id" value='.$category_id.'>
+                <input type="submit" id="submit" value="New Note">
+                </form>';
+
+                    }
+
+                } else {
+                    echo "Categories";
+                }
+
+
+                ?>
                 <button id="showFormButton" onclick="">New Category</button>
                 <form id="hiddenForm" method="post" style="display: none">
                     <input type="hidden" name="form_type" value="new_category">
                     <input type="hidden" name="group_id" value="<?php echo $group_id ?>">
-                    <input type="text" name="category">
+                    <input type="text" name="category" required>
                     <input type="submit" id="submit" value="submit">
                 </form>
+
             </h2> <!--název bude vždy categories - jméno aktegorie, ve které zrovna jsme -->
             <div class="dropdown-content">
                 <?php
@@ -116,7 +159,7 @@ if (!empty($_POST['form_type'])){
                     foreach ($categories as $category){
                         $category_name = $category['category_name'];
                         $category_id = $category['category_id'];
-                        echo"<a href='#category=$category_id'>$category_name</a>";
+                        echo"<a href='group.php?group_id=$group_id&category=$category_id'>$category_name</a>";
                     }
                 }
                 ?>
