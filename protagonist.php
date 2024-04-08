76aec1271df07d4203f756f167ac0432f51b92a9
Zuzana Osobová <osoz00@vse.cz>
2024-04-08 20:08:53 +0200

    Class and trait finished

--- protagonist.php
+++ protagonist.php
@@ -6,15 +6,15 @@ include "database_connection.inc.php";
 
 //TODO
 /* DONE - Character name
- * character class - SQL + dropdown form
+ * DONE - character class - SQL + dropdown form
  * character trait +- a základ na základě vybraného archetypu a počítadlo, kolik jich ještě mají a musí přidat
  * --
  * character info - základ textarea s doplěním věku, profese, title, vztahu a přezdívky (poznámka, nic z toho není povinné, ale alespoň něco)
- * character fyzický popis - text area
+ * DONE - character fyzický popis - text area
  * DONE - character mementos
  * DONE but gotta make CSS - character flaw - text area, ale možnost dropdown si vybrat???
  * DONE - character dilema
- * character background
+ * DONE - character background
  * --bude nastaveno samo--
  * character readies - nastaví se na začátku automaticky podle knihy
  * character stanting - mastaví se na začátku automaticky 0
@@ -48,7 +48,7 @@ include "database_connection.inc.php";
             <?php endif; ?>
             <input type="text" id="protagonist_name" name="protagonist_name" value="" required><br>
 
-            <label for="protagonist_class"><b>Choose a class:</b></label><br> <!-- přidat základní hodnotu aristocrat a přidat php pro classy -->
+            <label for="protagonist_class"><b>Choose a class:</b><br>Choose wisely, because your traits will change based on your class, and if you want to change them, you will have to click everything again.</label><br> <!-- přidat základní hodnotu aristocrat a přidat php pro classy -->
 
             <?php
             $query = $db ->prepare("SELECT * FROM archetypes");
@@ -95,10 +95,52 @@ include "database_connection.inc.php";
     </div>
 
     <div class="column">
+        <!-- přidat trait maker, responsive pomocí javascriptu -->
+        <p><b>Your Traits</b></p>
+        <p>Assing 5 points to traits of your choice.</p>
+        <?php
+        $query = $db ->prepare("SELECT * FROM traits");
+        $query->execute();
+        $traits = $query ->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach ($traits as $trait){
+            $trait_id = $trait['trait_id'];
+            $trait_name = $trait['trait_name'];
+            $trait_text = $trait['trait_text'];
+            $trait_number = "";
+
+            echo"
+            <label for='trait_$trait_id'><b>$trait_name</b></label>
+            <input type='number' name='trait_$trait_id' id='trait_$trait_id' min='1' max='5' value='1'><br>
+            <p id='trait_description'>$trait_text</p>
+            
+            ";
+
+
+        }
+
+        ?>
+
+
+
+        <label for="protagonist_background"><b>What is your background?</b><br>While your selected class will describe who your character is, your background will describe your upbringing and what events led you to where you are today.</label><br>
+        <textarea name="protagonist_background" id="protagonist_background"></textarea><br>
+
+        <label for="protagonist_info"><b>Tell us more about yourself:</b></label><br>
+        <textarea name="protagonist_info" id="protagonist_info">
+Age:
+Profession:
+Title:
+Nickname:
+Relationship: (single/married/it's complicated and who is it)
+        </textarea>
 
     </div>
 
     <div class="column">
+
+        <label for="protagonist_description"><b>How do you look?</b><br>Write out any distinct features for your Protagonist here.</label>
+        <input type="text" name="protagonist_description" id="protagonist_description"><br>
 
         <label for="protagonist_dilemma"><b>What is your dilema?</b><br>The dilema is na all-consumming burden that is a current fixture in your Protagonist's life, and at some point, sooner rather than later, they will have to adress it.</label><br>
         <input type="text" name="protagonist_dilemma" id="protagonist_dilemma"><br>
